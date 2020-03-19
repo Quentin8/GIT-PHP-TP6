450ccce1fb4ab8b3ddc889a165946d302a384b44
Qant1 <51907758+Quentin8@users.noreply.github.com>
2020-03-19 11:51:58 +0100

    Commit Question 3

--- TP6-PHP-Fournier.php
+++ TP6-PHP-Fournier.php
@@ -1,1 +1,22 @@
+<h1>TP6</h1>
+<hr>
+<h2>Exercice 1</h2>
+<hr>
 <?php
+    echo "<h3>Question 1</h3>";
+    class equipe {
+        public $nom;
+        public $nombreTitres;
+        function display(){
+            echo "L'équipe " . $this->nom . " a " . $this->nombreTitres . " de titres";
+        }
+    }
+    echo "<h3>Question 2</h3>";
+    $e1 = new equipe();
+    $e1->nom = "Equipe_1";
+    $e1->nombreTitres = 2;
+    $e1->display();
+    echo "<h3>Question 3</h3>";
+
+
+?>
